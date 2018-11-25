9610bab1a84561ffb156410e734aafd5b98cc999
Pjeferson <paulojeferson34@gmail.com>
2018-11-25 10:32:31 -0300

    adding attributes to deal with distance algorithms

--- src/graphexercises/graph/Vertex.java
+++ src/graphexercises/graph/Vertex.java
@@ -13,13 +13,31 @@ package graphexercises.graph;
 public class Vertex {
     private int key;
     private double value;
+    
+    //Maze solving
+    private char c;
+    private int x;
+    private int y;
+    private int h;
+    private int g;
+    private int f;
+    //
+    
+    public Vertex(int key, char c, int x,int y, int h, int g, int f) {
+        this.key = key;
+        this.c = c;
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.g = g;
+        this.f = f;
+    }
 
     public Vertex(int key, double value) {
-        super();
         this.key = key;
         this.value = value;
     }
-
+    
     public int getKey() {
         return key;
     }
@@ -36,4 +54,51 @@ public class Vertex {
         this.value = value;
     }
    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+    
+    public char getC() {
+        return c;
+    }
+
+    public void setC(char c) {
+        this.c = c;
+    }
 }
